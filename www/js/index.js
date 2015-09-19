d11bc72fc35203c1e54de7e0c48125c946bb536b
themaxtech <maxtechapps@gmail.com>
2015-09-19 17:32:13 +0530

    A3
    A3

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,127 @@
-var userHandler = {
+ /*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+ var userHandler = {
     username    : '', 
     userpass    : '',
     userclass   : '',
     usersec     : '',
     useroriname : '',
-    status      : ''
+    status      : '',
+    appid       : ''
 }
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        //var parentElement = document.getElementById(id);
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
+
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
+
+        //console.log('Received Event: ' + id);
+        var pushNotification = window.plugins.pushNotification;
+        if (device.platform == 'android' || device.platform == 'Android') {
+            //alert("Register called");
+            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"824841663931","ecb":"app.onNotificationGCM"});
+        }
+        else {
+            //alert("Register called");
+            pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
+        }
+    },
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+        userHandler.appid = result;
+        //alert('Callback Success! Result = '+result);
+
+    },
+    errorHandler:function(error) {
+        alert(error);
+    },
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    //alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              //alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+    },
+    onNotificationAPN: function(event) {
+        var pushNotification = window.plugins.pushNotification;
+        //alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
+        
+        if (event.alert) {
+            navigator.notification.alert(event.alert);
+        }
+        if (event.badge) {
+            pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge);
+        }
+        if (event.sound) {
+            var snd = new Media(event.sound);
+            snd.play();
+        }
+    }
+};
+
 
 $(document).on('pagecontainershow', function (e, ui) {
     var activePage = $(':mobile-pagecontainer').pagecontainer('getActivePage');
-    if(activePage.attr('id') === 'login') {
+    if(activePage.attr('id') === 'login') {  
+        
         $(document).on('click', '#submit', function() { // catch the form's submit event
             if($('#username').val().length > 0 && $('#password').val().length > 0){
              
@@ -20,7 +132,7 @@ $(document).on('pagecontainershow', function (e, ui) {
                 
                     $.ajax({url: 'http://themaxtech.com/app/auth.php',
                     //$.ajax({url: 'auth.php',
-                    data: {action : 'authorization', formData : $('#check-user').serialize()},
+                    data: {action : 'authorization', deviceid: userHandler.appid, formData : $('#check-user').serialize()},
                     type: 'post',                  
                     async: 'true',
                     dataType: 'json',
@@ -237,8 +349,8 @@ $(document).on('pagecontainershow', function (e, ui) {
                         //$.mobile.pageContainer.pagecontainer("change", "#year_calendar", { options });
 
 
-                         console.log($('.ui-page-active').attr('id')); 
-                         console.log('ARUN Log'); 
+                         //console.log($('.ui-page-active').attr('id')); 
+                         //console.log('ARUN Log'); 
                         
                         
                          
@@ -408,8 +520,8 @@ $(document).on('pagecontainershow', function (e, ui) {
                         "<h2>Home Work</h2></div><div data-role='content'>" + 
                         "</div><div data-role='footer'><p>  &copy;  2015 www.themaxtech.com</p></div></div>"; 
 
-                        $.mobile.activePage.after(stud_prof);
-                        //$.mobile.changePage("#year_calendar"); 
+                        //$.mobile.activePage.after(stud_prof);
+                        $.mobile.changePage("#year_calendar"); 
                         $.mobile.changePage( "#year_calendar", {transition: "none", reloadPage:false} );
                           function myFunctiond() { 
          
@@ -1364,8 +1476,8 @@ $(document).off('click', '#leavelistsubmit').on('click', '#leavelistsubmit',func
              if (tos === '#arunhome' && userHandler.status === 'success') {
              //   if (tos == '#unhome') {
                  
-               console.log("Final"); 
-               console.log($('.ui-page-active').attr('id')); 
+               //console.log("Final"); 
+               //console.log($('.ui-page-active').attr('id')); 
 
                $( ".ui-content" ).remove();  
                 //alert('am 2222clicked!');
@@ -1388,4 +1500,4 @@ $(document).off('click', '#leavelistsubmit').on('click', '#leavelistsubmit',func
     
 
 });
-
+ 
